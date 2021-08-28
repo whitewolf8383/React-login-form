c0116cd55322dde6c589a020e3d3067df8a2e131
whitewolf8383 <kandt.vandf@gmail.com>
2021-08-27 20:05:17 -0600

    Built Login Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+// Components
+import Login from './Login';
+
+let loggedIn = false;
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className="container">
+      {loggedIn ? <h1>Hello</h1> : <Login />}
+    </div>
   );
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// Components
+import Input from './Input';
+
+function Login () {
+  return(
+    <form className="form">
+      <Input type="text" placeholder="Username" />
+      <Input type="Password" placeholder="Password" />
+      <Input type="Password" placeholder="Confirm Password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
